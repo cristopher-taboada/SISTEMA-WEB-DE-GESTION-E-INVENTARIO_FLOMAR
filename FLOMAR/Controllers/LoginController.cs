6228505e45ae9f117c0bb16c62c4106c46feeff6
BrayanShell <brayanfloresmarzana@gmail.com>
2026-09-15 04:01:19 -0400

    Avance de Login y paneles
    Se agregaron las vistas iniciales de Login, Administrador y Vendedor.

--- FLOMAR/Controllers/LoginController.cs
+++ FLOMAR/Controllers/LoginController.cs
@@ -1,12 +1,39 @@
+<<<<<<< Updated upstream
 ﻿using Microsoft.AspNetCore.Mvc;
+=======
+﻿
+using FLOMAR.Models;
+using Microsoft.AspNetCore.Mvc;
+>>>>>>> Stashed changes
 
 namespace FLOMAR.Controllers
 {
     public class LoginController : Controller
     {
+<<<<<<< Updated upstream
+=======
+        [HttpGet]
+>>>>>>> Stashed changes
         public IActionResult Index()
         {
             return View();
         }
+<<<<<<< Updated upstream
     }
 }
+=======
+
+        [HttpPost]
+        public IActionResult Index(LoginViewModel modelo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(modelo);
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
+
+>>>>>>> Stashed changes
---
+++ FLOMAR/Models/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace FLOMAR.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string Usuario { get; set; } = string.Empty;
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Contrasena { get; set; } = string.Empty;
+    }
+}
