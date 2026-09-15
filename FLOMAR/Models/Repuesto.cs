5299d025528cd2e36408fb078768f2189c0fad0c
brayan217 <brayanmamanihidalgo203@gmail.com>
2026-09-15 01:55:23 -0400

    Update Repuesto.cs

--- FLOMAR/Models/Repuesto.cs
+++ FLOMAR/Models/Repuesto.cs
@@ -14,8 +14,8 @@ namespace FLOMAR.Models
         [Required(ErrorMessage = "El nombre es obligatorio")]
         [StringLength(150, ErrorMessage = "El nombre no puede tener más de 150 caracteres")]
         [Display(Name = "Nombre")]
-        public string Codigo { get; set; } = string.Empty;
-        public string Nombre { get; set; } = string.Empty;
+        public string Nombre { get; set; } = string.Empty;   
+
         public int Id_categoria { get; set; }
         public decimal costo_adquisicion { get; set; }
         public decimal precio_venta { get; set; }
