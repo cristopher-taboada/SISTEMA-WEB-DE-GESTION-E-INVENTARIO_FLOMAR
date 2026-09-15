2949a51ad583f941d78c6d036749e1399bd64405
brayan217 <brayanmamanihidalgo203@gmail.com>
2026-09-14 23:47:39 -0400

    completo

--- FLOMAR/Controllers/RepuestosController.cs
+++ FLOMAR/Controllers/RepuestosController.cs
@@ -5,25 +5,26 @@ namespace FLOMAR.Controllers
 {
     public class RepuestosController : Controller
     {
+        private static List<Repuesto> repuestos = new List<Repuesto>
+        {
+            new Repuesto
+            {
+                Id = 1,
+                Codigo = "REP-001",
+                Nombre = "Pastilla de freno"
+            },
+
+            new Repuesto
+            {
+                Id = 2,
+                Codigo = "REP-002",
+                Nombre = "Filtro de aceite"
+            }
+        };
+
+
         public IActionResult Index()
         {
-            var repuestos = new List<Repuesto>
-            {
-                new Repuesto
-                {
-                    Id = 1,
-                    Codigo = "REP-001",
-                    Nombre = "Pastilla de freno"
-                },
-
-                new Repuesto
-                {
-                    Id = 2,
-                    Codigo = "REP-002",
-                    Nombre = "Filtro de aceite"
-                }
-            };
-
             return View(repuestos);
         }
 
@@ -33,6 +34,7 @@ namespace FLOMAR.Controllers
             return View();
         }
 
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Create(Repuesto repuesto)
@@ -40,10 +42,71 @@ namespace FLOMAR.Controllers
             if (ModelState.IsValid)
             {
 
+                if (repuestos.Count > 0)
+                {
+                    repuesto.Id = repuestos.Max(r => r.Id) + 1;
+                }
+                else
+                {
+                    repuesto.Id = 1;
+                }
+
+                repuestos.Add(repuesto);
+
                 return RedirectToAction(nameof(Index));
             }
 
             return View(repuesto);
+        }
+
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var repuesto = repuestos.FirstOrDefault(r => r.Id == id);
+
+            if (repuesto == null)
+            {
+                return NotFound();
+            }
+
+            return View(repuesto);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Repuesto repuesto)
+        {
+            if (ModelState.IsValid)
+            {
+                var repuestoExistente =
+                    repuestos.FirstOrDefault(r => r.Id == repuesto.Id);
+
+                if (repuestoExistente == null)
+                {
+                    return NotFound();
+                }
+
+                repuestoExistente.Codigo = repuesto.Codigo;
+                repuestoExistente.Nombre = repuesto.Nombre;
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(repuesto);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Desactivar(int id)
+        {
+            var repuesto = repuestos.FirstOrDefault(r => r.Id == id);
+
+            if (repuesto != null)
+            {
+                repuestos.Remove(repuesto);
+            }
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
---
+++ FLOMAR/Views/Repuestos/Edit.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FLOMAR.Views.Repuestos
+{
+    public class EditModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
