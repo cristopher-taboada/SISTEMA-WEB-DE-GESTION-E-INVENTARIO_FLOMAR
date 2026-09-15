dd9b0cc81b54b79037e70563334f14cd401cba2a
brayan217 <brayanmamanihidalgo203@gmail.com>
2026-09-14 20:02:11 -0400

    Crear controlador y vista inicial de Repuestos

---
+++ FLOMAR/Views/Repuestos/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FLOMAR.Views.Repuestos
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
