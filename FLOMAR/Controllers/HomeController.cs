0fad8f3f268752726646eb3eccbd4affa4ac0568
BrayanShell <brayanfloresmarzana@gmail.com>
2026-09-22 18:45:42 -0400

    Buscador estático de vendedor
    Buscador estático de vendedor

--- FLOMAR/Controllers/HomeController.cs
+++ FLOMAR/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using FLOMAR.Models;
 
 namespace FLOMAR.Controllers
 {
@@ -15,10 +18,27 @@ namespace FLOMAR.Controllers
             return View();
         }
 
-        public IActionResult Vendedor()
+        public IActionResult Vendedor(string textoBusqueda)
         {
             if (LoginController.RolActual != 2) return RedirectToAction("Index", "Login");
-            return View();
+
+            var repuestosTemp = new List<Repuesto>
+            {
+                new Repuesto { Nombre = "Aro Enkei", Codigo = "AR-001", PrecioVenta = 850 },
+                new Repuesto { Nombre = "Disco de Freno", Codigo = "FR-002", PrecioVenta = 320 },
+                new Repuesto { Nombre = "Filtro de Aceite", Codigo = "FI-003", PrecioVenta = 75 },
+                new Repuesto { Nombre = "Pastillas de Freno", Codigo = "FR-004", PrecioVenta = 180 },
+                new Repuesto { Nombre = "Aro Deportivo", Codigo = "AR-006", PrecioVenta = 720 }
+            };
+
+            if (!string.IsNullOrEmpty(textoBusqueda))
+            {
+                repuestosTemp = repuestosTemp.Where(r =>
+                    r.Nombre.ToLower().Contains(textoBusqueda.ToLower()) ||
+                    r.Codigo.ToLower().Contains(textoBusqueda.ToLower())).ToList();
+            }
+
+            return View(repuestosTemp);
         }
     }
 }
