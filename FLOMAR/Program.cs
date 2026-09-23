74d56e21f8b6f44c5ed1557e20b183c86af3e9de
cristopher-taboada <cris341721@gmail.com>
2026-09-23 13:20:25 -0400

    Login

--- FLOMAR/Program.cs
+++ FLOMAR/Program.cs
@@ -3,7 +3,6 @@ using FLOMAR.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<FlomarContext>(options =>
@@ -11,11 +10,9 @@ builder.Services.AddDbContext<FlomarContext>(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -28,8 +25,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=Login}/{action=Index}/{id?}")
     .WithStaticAssets();
-
 
 app.Run();
